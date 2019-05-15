718fbc5041e519976875939d2d7c04ea020c4d32
Tanay Pratap <tanay.mit@gmail.com>
2019-05-16 00:31:19 +0530

    add text to curriculum

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import pic02 from '../assets/images/pic02.jpg'
 
 const BannerLanding = (props) => (
-    <section id="banner" className="style2">
+    <section id="banner" className="style2" style={{ backgroundImage: `url(${pic02})` }}>
         <div className="inner">
             <header className="major">
-                <h1>Coming Soon!</h1>
+                <h2>Detailed Curriculum Coming Soon!</h2>
             </header>
             <div className="content">
-                <p>Course will make you a fullstack developer equipped with ML knowledge. Details are coming sooner than you expect</p>
+                <p>Project based mini courses, everything you learn goes on your portfolio, on your github account</p>
+                <p>Aimed at making you a full stack developer ninja with ML skills </p>
+                <p>JavaScript, Python, HTML, CSS, Typescript</p>
+                <p>Data structures and algorithms, test driven development, open source contributions, system design, security and performance optimisation</p>
             </div>
         </div>
     </section>
