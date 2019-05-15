fafd06f7fc93eb2e2537e5f67e126248698ce159
Tanay Pratap <tanay.mit@gmail.com>
2019-05-16 00:31:30 +0530

    telegram link add

--- src/components/Footer.temp.js
+++ src/components/Footer.temp.js
@@ -8,7 +8,7 @@ const Footer = (props) => (
                 <li><a target="_blank" href="https://facebook.com/tanaypratap.official" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                 <li><a target="_blank" href="https://instagram.com/tanaypratap" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                 <li><a target="_blank" href="https://github.com/learncodingfree" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a target="_blank" href="https://www.linkedin.com/in/tpratap/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a target="_blank" href="https://t.me/tanaypratap/" className="icon alt fa-telegram"><span className="label">Telegram</span></a></li>
             </ul>
         </div>
     </footer>
