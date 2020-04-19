c3569f1cb65ae4af3fb547823f25365f651ad424
Tanay Pratap <tanay.mit@gmail.com>
2020-04-20 01:26:22 +0530

    adding jobChallenge
    Some minor changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,44 @@
 import React from 'react'
 
-const Banner = (props) => (
-    <section id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h1>learncodingfree.org</h1>
-            </header>
-            <div className="content">
-                <p>Join a community of 20,000+ students from 50+ countries! <br />Learn how to be a full stack developer ninja <br />with Tanay Pratap, Engineer@Microsoft</p>
-
-                <ul className="actions">
-                    <li><a href="#html-course" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>learncodingfree.org</h1>
+      </header>
+      <div className="content">
+        <p>
+          Join a community of 20,000+ students from 50+ countries! <br />
+        </p>{' '}
+        <p>
+          Learn how to be a full stack developer ninja with Tanay Pratap,
+          Engineer@Microsoft
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#html-course" className="button next scrolly">
+              Get Started
+            </a>
+          </li>
+        </ul>
+        <p>
+          {' '}
+          Hey! Do checkout out <em>jobchallenge.dev</em> if you're looking for a
+          job or internship as a fresher. Registrations open till June 1st, 2020
+        </p>
+        <ul className="actions">
+          <li>
+            <a
+              href="http://2020.teamtanay.jobchallenge.dev/"
+              className="button next scrolly"
+            >
+              take the job challenge
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 )
 
 export default Banner
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,92 +12,156 @@ import pic05 from '../assets/images/pic05.jpg'
 import pic06 from '../assets/images/pic06.jpg'
 
 class HomeIndex extends React.Component {
-    render() {
+  render() {
+    return (
+      <Layout>
+        <Helmet
+          title="learncodingfree.org"
+          meta={[
+            {
+              name: 'description',
+              content: 'JOIN A COMMUNITY OF 20,000+ STUDENTS FROM 50+ COUNTRIES! LEARN HOW TO BE A FULL STACK DEVELOPER NINJA WITH TANAY PRATAP, ENGINEER@MICROSOFT'.toLowerCase(),
+            },
+            { name: 'keywords', content: 'react, javascript, data structures' },
+            {
+              property: 'og:image',
+              content: `https://i.imgur.com/QsB7kWS.png`,
+            },
+          ]}
+        >
+          <meta
+            property="og:image"
+            content={`https://i.imgur.com/QsB7kWS.png`}
+          />
+        </Helmet>
 
-        return (
-            <Layout>
-                <Helmet
-                    title="learncodingfree.org"
-                    meta={[
-                        {
-                            name: 'description', content: 'JOIN A COMMUNITY OF 20,000+ STUDENTS FROM 50+ COUNTRIES! LEARN HOW TO BE A FULL STACK DEVELOPER NINJA WITH TANAY PRATAP, ENGINEER@MICROSOFT'.toLowerCase()
-                        },
-                        { name: 'keywords', content: 'react, javascript, data structures' },
-                        { property: 'og:image', content: `https://i.imgur.com/QsB7kWS.png` }
-                    ]}
+        <Banner />
+
+        <div id="main">
+          <section id="one" className="tiles">
+            <article
+              style={{
+                backgroundImage: `url(https://pbs.twimg.com/profile_banners/52322389/1563764744/1500x500)`,
+              }}
+            >
+              <header className="major">
+                <h3>your instructor</h3>
+                <p>Tanay Pratap, Engineer @Microsoft</p>
+              </header>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/tanaypratap/"
+                className="link primary"
+              />
+            </article>
+            <article style={{ backgroundImage: `url(${pic02})` }}>
+              <header className="major">
+                <h3>curriculum</h3>
+                <p>
+                  javascript, reactjs, pwa, node, mongo, bootstrap, cloud,
+                  machine learning, python, test driven development, data
+                  structures, algorithms and many more to come
+                </p>
+              </header>
+              <Link to="/curriculum" className="link primary" />
+            </article>
+            <article
+              id="html-course"
+              style={{ backgroundImage: `url(${pic03})` }}
+            >
+              <header className="major">
+                <h3>start the course</h3>
+                <p>
+                  learn html css while creating your own portfolio and hosting
+                  it
+                </p>
+                <p>
+                  you will have your own web address once you're done with this
+                </p>
+                <p>
+                  it's smart practice to subscribe to the channel so that you
+                  don't miss future lessons
+                </p>
+              </header>
+              <a
+                target="_blank"
+                href="https://www.youtube.com/playlist?list=PLzvhQUIpvvug-c-bExl_xFcopeQi_sa29"
+                className="link primary"
+              />
+            </article>
+            <article style={{ backgroundImage: `url(${pic04})` }}>
+              <header className="major">
+                <h3>learn everyday, join the community</h3>
+                <p>
+                  an open course, built with your feedback, participate and ask
+                  questions
+                </p>
+                <p>A dedicated instagram account only for this course </p>
+              </header>
+              <a
+                target="_blank"
+                href="https://instagram.com/tanaypratap"
+                className="link primary"
+              />
+            </article>
+            <article style={{ backgroundImage: `url(${pic05})` }}>
+              <header className="major">
+                <h3>frequently asked questions (faq)</h3>
+                <p>
+                  We know you'd have questions, check if it's already answered
+                </p>
+              </header>
+
+              <a
+                target="_blank"
+                href="https://www.youtube.com/playlist?list=PLzvhQUIpvvuhBNrOVqlhRnhDqz8ob6MeW"
+                className="link primary"
+              />
+            </article>
+            <article style={{ backgroundImage: `url(${pic06})` }}>
+              <header className="major">
+                <h3>ask me anything (ama)</h3>
+                <p>
+                  If you don't find an answer on the AMA playlist, chances are
+                  it's unique.
+                </p>
+                <p>
+                  Put your question then in the Github AMA repository, go
+                  through README you'll understand what to do
+                </p>
+              </header>
+              <a
+                target="_blank"
+                href="https://bit.ly/tanay-ama"
+                className="link primary"
+              />
+            </article>
+          </section>
+          <section id="two">
+            <div className="inner">
+              <header className="major">
+                <h2>Got stuck with course somewhere?</h2>
+              </header>
+              <p>
+                Let's build a community where you can get your doubts related to
+                the course answered and you help others to move forward
+              </p>
+              <ul className="actions">
+                <a
+                  target="_blank"
+                  href="https://t.me/teamtanay"
+                  className="button next"
                 >
-                    <meta property="og:image" content={`https://i.imgur.com/QsB7kWS.png`} />
-                </Helmet>
-
-
-                <Banner />
-
-                <div id="main">
-                    <section id="one" className="tiles">
-                        <article style={{ backgroundImage: `url(${pic01})` }}>
-                            <header className="major">
-                                <h3>your instructor</h3>
-                                <p>Tanay Pratap, Engineer @Microsoft</p>
-                            </header>
-                            <a target="_blank" href="https://www.linkedin.com/in/tpratap/" className="link primary"></a>
-                        </article>
-                        <article style={{ backgroundImage: `url(${pic02})` }}>
-                            <header className="major">
-                                <h3>curriculum</h3>
-                                <p>javascript, reactjs, pwa, node, mongo, bootstrap, cloud, machine learning, python, test driven development, data structures, algorithms and many more to come</p>
-                            </header>
-                            <Link to="/curriculum" className="link primary"></Link>
-                        </article>
-                        <article id="html-course" style={{ backgroundImage: `url(${pic03})` }}>
-                            <header className="major">
-                                <h3>start the course</h3>
-                                <p>learn html css while creating your own portfolio and hosting it</p>
-                                <p>you will have your own web address once you're done with this</p>
-                                <p>it's smart practice to subscribe to the channel so that you don't miss future lessons</p>
-                            </header>
-                            <a target="_blank" href="https://www.youtube.com/playlist?list=PLzvhQUIpvvug-c-bExl_xFcopeQi_sa29" className="link primary"></a>
-                        </article>
-                        <article style={{ backgroundImage: `url(${pic04})` }}>
-                            <header className="major">
-                                <h3>learn everyday, join the community</h3>
-                                <p>an open course, built with your feedback, participate and ask questions</p>
-                                <p>A dedicated instagram account only for this course </p>
-                            </header>
-                            <a target="_blank" href="https://instagram.com/tanaypratap" className="link primary"></a>
-                        </article>
-                        <article style={{ backgroundImage: `url(${pic05})` }}>
-                            <header className="major">
-                                <h3>frequently asked questions (faq)</h3>
-                                <p>We know you'd have questions, check if it's already answered</p>
-                            </header>
-
-                            <a target="_blank" href="https://www.youtube.com/playlist?list=PLzvhQUIpvvuhBNrOVqlhRnhDqz8ob6MeW" className="link primary"></a>
-                        </article>
-                        <article style={{ backgroundImage: `url(${pic06})` }}>
-                            <header className="major">
-                                <h3>ask me anything (ama)</h3>
-                                <p>If you don't find an answer on the AMA playlist, chances are it's unique.</p>
-                                <p>Put your question then in the Github AMA repository, go through README you'll understand what to do</p>
-                            </header>
-                            <a target="_blank" href="https://bit.ly/tanay-ama" className="link primary"></a>
-                        </article>
-                    </section>
-                    <section id="two">
-                        <div className="inner">
-                            <header className="major">
-                                <h2>Got stuck with course somewhere?</h2>
-                            </header>
-                            <p>Let's build a community where you can get your doubts related to the course answered and you help others to move forward</p>
-                            <ul className="actions">
-                                <a target="_blank" href="https://www.facebook.com/groups/328710937827045" className="button next"> JOIN THE FB GROUP</a>
-                            </ul>
-                        </div>
-                    </section>
-                </div>
-
-            </Layout>
-        )
-    }
+                  {' '}
+                  JOIN THE TELEGRAM GROUP
+                </a>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default HomeIndex
